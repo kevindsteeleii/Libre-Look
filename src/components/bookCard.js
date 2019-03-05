a804adcbfa1ee93a68d1370b30aa31986fbb2de2
github username <github email address>
2019-03-05 02:45:03 -0500

    fixed unsecured thumbnail issue

--- src/components/bookCard.js
+++ src/components/bookCard.js
@@ -8,7 +8,7 @@ const BookCard = (props) => {
   console.log(props.book);
   return(<div className="book-card">
     {/* will need to incorporate the individual link to indicate when to swap out the picture etc. for responsiveness */}
-    <img className='thumbnail' src={volumeInfo.imageLinks.thumbnail} alt={`${volumeInfo.title} Book Cover`}/>
+    <img className='thumbnail' src={`${volumeInfo.imageLinks.thumbnail.replace('http','https')}`} alt={`${volumeInfo.title} Book Cover`}/>
     <div className="info-dump text">
       <p className="book-title text">{volumeInfo.title}</p>
       <p className="author text">By: <span>{author}</span></p>
